9142bd8f51c5c99ad920d549ea762bf88d8a4dac
Ilia Ablamonov <ilia@flamefork.ru>
2012-09-01 15:48:40 +0400

    Start ec2 instance if needed

--- src/hrm_task.erl
+++ src/hrm_task.erl
@@ -9,6 +9,8 @@
 
 -record(state, {id, task}).
 
+-define(EC2_INTERVAL, 3000).
+
 start_link(TaskId, Task) ->
     gen_server:start_link(?MODULE, [TaskId, Task], []).
 
@@ -16,31 +18,51 @@ init([TaskId, Task]) ->
     {ok, #state{id=TaskId, task=Task}, 0}.
 
 handle_info(_Info, #state{id=TaskId, task=Task}=State) ->
-    do_action_request(TaskId, Task),
+    ok = ensure_instance(Task),
+    ok = do_action_request(TaskId, Task),
     {stop, normal, State}.
 
 do_action_request(TaskId, Task) ->
     Url = hrm_utils:append_query_params(hrm_tasks:field_value(action_url, Task), [{hrm_task_id, TaskId}]),
     {ok, {{_, StatusCode, StatusText}, _, _}} = httpc:request(Url),
-    process_action_response({StatusCode, StatusText}, TaskId),
+    {ok, Task2} = if
+      StatusCode == 200 ->
+        hrm_storage:append(TaskId, [{status, complete}]);
+      true ->
+        Meta = {[{status, StatusCode}, {message, list_to_binary(StatusText)}]},
+        hrm_storage:append(TaskId, [{status, error}, {meta, Meta}])
+    end,
+    Params = [{hrm_task_id, TaskId}, {hrm_task, jiffy:encode({Task2})}],
+    Url2 = hrm_utils:append_query_params(hrm_tasks:field_value(callback_url, Task2), Params),
+    {ok, _} = httpc:request(Url2),
     ok.
 
-process_action_response({200, _}, TaskId) ->
-    {ok, Task2} = hrm_storage:append(TaskId, [{status, complete}]),
-    ok = do_callback(TaskId, Task2),
-    ok;
+ensure_instance(Task) ->
+  case hrm_tasks:field_value(instance_id, Task) of
+    undefined ->
+      ok;
+    InstanceId ->
+      EC2 = erlaws_ec2:new(hrm_tasks:field_value(access_key_id, Task), hrm_tasks:field_value(access_key_secret, Task), true),
+      wait_for_instance(InstanceId, EC2)
+  end.
 
-process_action_response({StatusCode, StatusText}, TaskId) ->
-    Meta = {[{status, StatusCode}, {message, list_to_binary(StatusText)}]},
-    {ok, Task2} = hrm_storage:append(TaskId, [{status, error}, {meta, Meta}]),
-    ok = do_callback(TaskId, Task2),
-    ok.
-
-do_callback(TaskId, Task) ->
-    Params = [{hrm_task_id, TaskId}, {hrm_task, jiffy:encode({Task})}],
-    Url = hrm_utils:append_query_params(hrm_tasks:field_value(callback_url, Task), Params),
-    {ok, _} = httpc:request(Url),
-    ok.
+wait_for_instance(InstanceId, EC2) ->
+  [InstanceData] = EC2:describe_instances([InstanceId]),
+  State = proplists:get_value(instance_state, InstanceData),
+  io:format("State: ~p~n", [State]),
+  case State of
+    "stopped" ->
+      EC2:start_instances([InstanceId]),
+      timer:sleep(?EC2_INTERVAL),
+      wait_for_instance(InstanceId, EC2);
+    "pending" ->
+      timer:sleep(?EC2_INTERVAL),
+      wait_for_instance(InstanceId, EC2);
+    "stopping" ->
+      timer:sleep(?EC2_INTERVAL),
+      wait_for_instance(InstanceId, EC2);
+    "running" -> ok
+  end.
 
 %% Boilerplate
 
